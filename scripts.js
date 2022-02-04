9b840d083e51b5f86d42c386cc33400a1dabc625
jpswt <joel_swt@yahoo.com>
2022-02-03 18:21:03 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let usersWord = ""
+let oddList = document.getElementById("odd")
+let evenList = document.getElementById("even")
+
+function updateWord(element) {
+    usersWord = element.value
+
+    console.log(usersWord)
+}
+
+function handleSubmit() {
+    let newListItem = document.createElement('LI')
+    newListItem.innerText = usersWord
+
+    /* compare remainder to 0 to see if even or not */
+    if(usersWord.length % 2 === 0) {
+        /*put word in even lists*/
+        evenList.appendChild(newListItem)
+      } else {
+        /*put word in odd list*/
+        oddList.appendChild(newListItem)
+        }
+
+    usersWord = ""
+    document.getElementById("even-odd-form").reset()
+  }
+
+  function runScript(e) {
+      if(e.keycode == 13) {
+          let tb = document.getElementById("word")
+          eval(tb.value)
+          return false
+      }
+  }
